e237e83647eab7d92d98cb502e41f7385c7dee96
Avner <avner.conceicao@hotmail.com>
2018-01-24 23:14:30 -0200

    Add lifecycle update internal

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,6 +28,19 @@ class App extends Component {
     console.log('[App.js] Inside ComponentDidMount.');
   }
 
+  shouldComponentUpdate (nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return true;
+}
+
+componentWillUpdate (nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+}
+
+componentDidUpdate () {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+}
+
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
